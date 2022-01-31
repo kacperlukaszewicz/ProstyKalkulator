6b708a389e4fae032ff7088d88126b1005e43bd8
Kacper Łukaszewicz <kacper7769@wp.pl>
2022-01-31 12:12:29 +0100

    Dodaj pliki projektów.

---
+++ ProstyKalkulator/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProstyKalkulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+            tb.Text += b.Content.ToString();
+        }
+
+        private void Result_click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                result();
+            }
+            catch (Exception exc)
+            {
+                tb.Text = "ERROR";
+            }
+        }
+
+        private void result()
+        {
+            String op;
+            int iOp = 0;
+            if (tb.Text.Contains("+"))
+            {
+                iOp = tb.Text.IndexOf("+");
+            }
+            else if (tb.Text.Contains("-"))
+            {
+                iOp = tb.Text.IndexOf("-");
+            }
+            else if (tb.Text.Contains("*"))
+            {
+                iOp = tb.Text.IndexOf("*");
+            }
+            else if (tb.Text.Contains("/"))
+            {
+                iOp = tb.Text.IndexOf("/");
+            }
+            else
+            {
+                //error    
+            }
+
+            op = tb.Text.Substring(iOp, 1);
+            double op1 = Convert.ToDouble(tb.Text.Substring(0, iOp));
+            double op2 = Convert.ToDouble(tb.Text.Substring(iOp + 1, tb.Text.Length - iOp - 1));
+
+            if (op == "+")
+            {
+                tb.Text += "=" + (op1 + op2);
+            }
+            else if (op == "-")
+            {
+                tb.Text += "=" + (op1 - op2);
+            }
+            else if (op == "*")
+            {
+                tb.Text += "=" + (op1 * op2);
+            }
+            else if (op == "/")
+            {
+                tb.Text += "=" + (op1 / op2);
+            }
+        }
+
+        private void Del_Click(object sender, RoutedEventArgs e)
+        {
+            if (tb.Text.Length > 0)
+            {
+                tb.Text = tb.Text.Substring(0, tb.Text.Length - 1);
+            }
+        }
+
+        private void C_Click(object sender, RoutedEventArgs e)
+        {
+            tb.Text = "";
+        }
+    }
+}
